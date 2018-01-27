febb1531dd9630340578b937a261d4425ab748f5
cool-cool-sweat <35659567+cool-cool-sweat@users.noreply.github.com>
2018-01-26 22:52:03 -0500

    Throw it out the window

---
+++ README.md
@@ -0,0 +1,150 @@
+## fenêtre
+
+awesome 4 widget that provides a global titlebar with buttons for closing, un/maximising completely/vertically/horizontally, + toggling ontop, sticky, + floating. The buttons also serve as indicators for the corresponding client property
+
+All elements're optional + can be arranged as you like
+
+## Basic use:
+```bash
+git clone https://github.com/cool-cool-sweat/fenetre ~/.config/awesome/fenetre
+```
+rc.lua:
+```lua
+awful.rules.rules = {
+...
+{ rule_any = { type = { "dialog", "normal" } }, properties = { titlebars_enabled = false } },
+...
+```
+theme.lua:
+```lua
+local fenetre = require("fenetre")
+local titlebar = fenetre { }
+```
+Then add it to your wibar
+
+## Available arguments
+
+Argument | Use | Type | Default
+--- | --- | --- | ---
+`order` | Elements to create + order to show them in | table of strings | `{ "close", "max", "ontop", "sticky", "floating", "separator", "title" }`
+`rotation` | If set to `left` or `right` the elements're arranged for a vertical wibar + the title's rotated facing left or right. If not set to `left` or `right` they're arranged for a horizontal wibar | string | None
+`clickable` | Allow clicking the buttons to change the corresponding client property | boolean | true
+`icons_dir` | Where to look for the icons to use | string | path_to_widget/icons/
+`close_icon` | Path of image to use for the close button | string | icons_dir/close.png
+`ontop_icon` | Path of image to use for the ontop button when the client's ontop | string | icons_dir/ontop.png
+`ontop_off_icon` | Path of image to use for the ontop button when the client's not ontop | string | icons_dir/ontop_off.png
+`sticky_icon` | Path of image to use for the sticky button when the client's sticky | string | icons_dir/sticky.png
+`sticky_off_icon` | Path of image to use for the sticky button when the client's not sticky | string | icons_dir/sticky_off.png
+`floating_icon` | Path of image to use for the floating button when the client's floating | string | icons_dir/floating.png
+`floating_off_icon` | Path of image to use for the floating button when the client's not floating | string | icons_dir/floating_off.png
+`max_icon` | Path of image to use for the maximise button when the client's fully maximised | string | icons_dir/max.png
+`max_partial_icon` | Path of image to use for the maximise button when the client's only vertically or horizontally maximised | string | icons_dir/max_partial.png
+`max_off_icon` | Path of image to use for the maximise button when the client's not maximised | string | icons_dir/max_off.png
+`mouse_button` | Mouse button to use for clicking | number | 1
+`max_vert_button` | Mouse button to use for toggle un/maximising the client vertically using the maximise button. If set to `Shift` or `Control` or `Alt` or `Mod4`, then `mouse_button` plus the specified key're used together | number or string | 2
+`max_horiz_button` | Mouse button to use for toggle un/maximising the client horizontally. If set to `Shift` or `Control` or `Alt` or `Mod4`, then `mouse_button` plus the specified key're used together | number or string | 3
+`title_font` | Font + size to use for the title | string | None, using awesome's default
+`title_color` | Colour to use for the title | string | "#FFFFFF"
+`title_edit` | Function to use to edit the title | function | None
+`separator` | Text to use as a separator | string | " "
+`separator_font` | Font + size to use for the separator if it's visible | string | None or `title_font` if it's set
+`separator_color` | Colour to use for the separator if it's visible | string | "#FFFFFF" or `title_color` if it's set
+
+## Notes
+The default icons're included in png + xpm format for easy customisation. You can quickly change the colours using bash/zsh, sed, + imagemagick:
+```bash
+cd path_to_widget/icons
+sed -i "s,#7ED68D,new_colour," {close,max_partial,*_off}.xpm
+sed -i "s,white,new_colour," {ontop,sticky,floating,max}.xpm
+for icon in *.xpm; do convert $icon ${icon%.xpm}.png; done
+```
+The default icons were originally sourced from https://github.com/lcpz/awesome-copycats + are the same designs except for the ontop icons, which were slightly modified
+
+Icons licence: [BY-NC-SA](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+
+## Usage examples
+```lua
+local titlebar = fenetre {
+    title_font = theme.font,
+    title_color = theme.fg_normal,
+    title_edit = function()
+        -- Remove " - Mozilla Firefox from the end of firefox's titles"
+        local firefox = " - Mozilla Firefox"
+        local pri_brow = firefox .. " (Private Browsing)"
+        if title:sub(-firefox:len()) == firefox or title:sub(-pri_brow:len()) == pri_brow then
+            title = title:gsub(" %- Mozilla Firefox", "")
+        end
+    end,
+    order = { "max", "ontop", "sticky", "floating", "title" }
+}
+```
+
+```lua
+local titlebar = fenetre {
+    max_vert_button = "Shift",
+    max_horiz_button = "Control",
+    title_font = theme.font,
+    title_color = theme.fg_normal,
+    order = { "max", "ontop", "sticky", "floating", "title" }
+}
+```
+
+```lua
+local titlebar = fenetre {
+    rotation = "right",
+    max_vert_button = "Shift",
+    max_horiz_button = "Control",
+    title_font = theme.font,
+    title_color = theme.fg_normal,
+    title_edit = function()
+        if title == "calc" then title = "calculator" end
+        title = title:upper()
+    end,
+    order = { "title", "separator", "ontop", "sticky", "floating", "max" }
+}
+```
+
+```lua
+local titlebar = fenetre {
+    clickable = false,
+    title_font = theme.font,
+    title_color = theme.fg_normal,
+    separator = "|",
+    order = { "title", "separator", "max", "separator", "ontop", "separator", "sticky", "separator", "floating" }
+}
+```
+
+```lua
+local titlebar = fenetre {
+    title_font = theme.font,
+    mouse_button = 5,
+    title_color = theme.fg_focus,
+    order = { "title", "separator", "max", "floating", "ontop", "sticky", "close" }
+}
+```
+
+```lua
+local titlebar = fenetre {
+    rotation = "left",
+    clickable = false,
+    icons_dir = theme.dir .. "icons/",
+    order = { "sticky", "floating", "ontop" }
+}
+```
+
+```lua
+local titlebar = fenetre {
+    rotation = "right",
+    max_vert_button = 3,
+    max_horiz_button = 2,
+    ontop_icon = file_name,
+    ontop_off_icon = file_name,
+    sticky_icon = file_name,
+    sticky_off_icon = file_name,
+    max_icon = file_name,
+    max_partial_icon = file_name,
+    max_off_icon = file_name,
+    title_font = "comic sans 69",
+    order = { "title", "max", "ontop", "sticky" }
+}
+```
---
+++ init.lua
@@ -0,0 +1,258 @@
+local layout = require("wibox.layout").fixed
+local imagebox = require("wibox.widget").imagebox
+local textbox = require("wibox.widget").textbox
+local rotate = require("wibox.container").rotate
+local button = require("awful.button")
+local join = require("gears.table").join
+local escape = require("awful.util").escape
+
+local function fenetre(args)
+    local args = args or {  }
+    local order = args.order or { "close", "max", "ontop", "sticky", "floating", "separator", "title" }
+    local icons_dir = args.icons_dir or debug.getinfo(1, 'S').source:match[[^@(.*/).*$]] .. "icons/"
+
+    local titlebar
+    if args.rotation == "left" or args.rotation == "right" then
+        titlebar = layout.vertical()
+    else
+        titlebar = layout.horizontal()
+    end
+
+    local clickable
+    if args.clickable == false then
+        clickable = false
+    else
+        local mouse_button = args.mouse_button or 1
+        clickable = true
+    end
+
+    for _, part in ipairs(order) do -- Only show elements included in order
+        if part == "close" then
+            local close_button = imagebox()
+            local close_icon = args.close_icon or icons_dir .. "close.png"
+
+            titlebar:add(close_button)
+
+            if clickable then
+                close_button:buttons(join(button({ }, mouse_button, function()
+                    if client.focus then client.focus:kill() end
+                end)))
+            end
+
+            client.connect_signal("focus", function() close_button:set_image(close_icon) end)
+            client.connect_signal("unfocus", function() close_button:set_image() end)
+
+        elseif part == "ontop" then
+            local ontop_button = imagebox()
+            local ontop_icon = args.ontop_icon or icons_dir .. "ontop.png"
+            local ontop_off_icon = args.ontop_off_icon or icons_dir .. "ontop_off.png"
+
+            local function update_ontop(c)
+                if c == client.focus then
+                    if c.ontop then
+                        ontop_button:set_image(ontop_icon)
+                    else
+                        ontop_button:set_image(ontop_off_icon)
+                    end
+                end
+            end
+
+            titlebar:add(ontop_button)
+
+            if clickable then
+                ontop_button:buttons(join(button({ }, mouse_button, function()
+                    if client.focus then
+                        client.focus.ontop = not client.focus.ontop
+                        update_ontop(client.focus)
+                    end
+                end)))
+            end
+
+            client.connect_signal("focus", update_ontop)
+            client.connect_signal("property::ontop", update_ontop)
+            client.connect_signal("unfocus", function() ontop_button:set_image() end)
+
+        elseif part == "sticky" then
+            local sticky_button = imagebox()
+            local sticky_icon = args.sticky_icon or icons_dir .. "sticky.png"
+            local sticky_off_icon = args.sticky_off_icon or icons_dir .. "sticky_off.png"
+
+            local function update_sticky(c)
+                if c == client.focus then
+                    if c.sticky then
+                        sticky_button:set_image(sticky_icon)
+                    else
+                        sticky_button:set_image(sticky_off_icon)
+                    end
+                end
+            end
+
+            titlebar:add(sticky_button)
+
+            if clickable then
+                sticky_button:buttons(join(button({ }, mouse_button, function()
+                    if client.focus then
+                        client.focus.sticky = not client.focus.sticky
+                        update_sticky(client.focus)
+                    end
+                end)))
+            end
+
+            client.connect_signal("focus", update_sticky)
+            client.connect_signal("property::sticky", update_sticky)
+            client.connect_signal("unfocus", function() sticky_button:set_image() end)
+
+        elseif part == "floating" then
+            local floating_button = imagebox()
+            local floating_icon = args.floating_icon or icons_dir .. "floating.png"
+            local floating_off_icon = args.floating_off_icon or icons_dir .. "floating_off.png"
+
+            local function update_floating(c)
+                if c == client.focus then
+                    if c.floating then
+                        floating_button:set_image(floating_icon)
+                    else
+                        floating_button:set_image(floating_off_icon)
+                    end
+                end
+            end
+
+            titlebar:add(floating_button)
+
+            if clickable then
+                floating_button:buttons(join(button({ }, mouse_button, function()
+                    if client.focus then
+                        client.focus.floating = not client.focus.floating
+                        update_floating(client.focus)
+                    end
+                end)))
+            end
+
+            client.connect_signal("focus", update_floating)
+            client.connect_signal("property::floating", update_floating)
+            client.connect_signal("unfocus", function() floating_button:set_image() end)
+
+        elseif part == "max" then
+            local max_button = imagebox()
+            local max_icon = args.max_icon or icons_dir .. "max.png"
+            local max_partial_icon = args.max_partial_icon or icons_dir .. "max_partial.png"
+            local max_off_icon = args.max_off_icon or icons_dir .. "max_off.png"
+
+            local function update_max(c)
+                if c == client.focus then
+                    if c.maximized or (c.maximized_vertical and c.maximized_horizontal) then
+                        max_button:set_image(max_icon)
+                    elseif c.maximized_vertical or c.maximized_horizontal then
+                        max_button:set_image(max_partial_icon)
+                    else
+                        max_button:set_image(max_off_icon)
+                    end
+                end
+            end
+
+            titlebar:add(max_button)
+
+            if clickable then
+                local max_vert_button = args.max_vert_button or 2
+                local max_horiz_button = args.max_horiz_button or 3
+
+                local max_vert_modifier
+                if max_vert_button == "Shift" or max_vert_button == "Control"
+                    or max_vert_button == "Mod4" or max_vert_button == "Alt" then
+                        max_vert_modifier = max_vert_button
+                        max_vert_button = mouse_button
+                end
+
+                local max_horiz_modifier
+                if max_horiz_button == "Shift" or max_horiz_button == "Control"
+                    or max_horiz_button == "Mod4" or max_horiz_button == "Alt" then
+                        max_horiz_modifier = max_horiz_button
+                        max_horiz_button = mouse_button
+                end
+
+                max_button:buttons(join(
+                    button({ }, mouse_button, function()
+                        if client.focus then
+                            client.focus.maximized = not client.focus.maximized
+                            update_max(client.focus)
+                        end
+                    end),
+
+                    button({ max_vert_modifier }, max_vert_button, function()
+                        if client.focus then
+                            client.focus.maximized_vertical = not client.focus.maximized_vertical
+                            update_max(client.focus)
+                        end
+                    end),
+
+                    button({ max_horiz_modifier }, max_horiz_button, function()
+                        if client.focus then
+                            client.focus.maximized_horizontal = not client.focus.maximized_horizontal
+                            update_max(client.focus)
+                        end
+                    end)))
+            end
+
+            client.connect_signal("focus", update_max)
+            client.connect_signal("property::maximized", update_max)
+            client.connect_signal("property::maximized_vertical", update_max)
+            client.connect_signal("property::maximized_horizontal", update_max)
+            client.connect_signal("unfocus", function() max_button:set_image() end)
+
+        elseif part == "title" then
+            local title_rotation
+            if args.rotation == "left" then
+                title_rotation = "east"
+            elseif args.rotation == "right" then
+                title_rotation = "west"
+            else
+                title_rotation = "north"
+            end
+
+            local client_title = rotate(textbox(), title_rotation)
+            local font = args.title_font or nil
+            local color = args.title_color or "#FFFFFF"
+
+            local function update_title(c)
+                if c == client.focus then
+                    title = nil
+                    if c.name then
+                        title = escape(c.name)
+                    elseif c.class then
+                        title = escape(c.class)
+                    end
+
+                    if args.title_edit then args.title_edit() end
+
+                    if title then
+                        client_title.widget.markup = string.format("<span font='%s' foreground='%s'>%s</span>", font, color, escape(title))
+                        -- escape(title) again in case the user adds any special characters thru title_edit
+                    end
+                end
+            end
+
+            titlebar:add(client_title)
+
+            client.connect_signal("focus", update_title)
+            client.connect_signal("property::name", update_title)
+            client.connect_signal("unfocus", function() client_title.widget.text = "" end)
+
+        elseif part == "separator" then
+            local separator = textbox()
+            local font = args.separator_font or args.title_font or nil
+            local color = args.separator_color or args.title_color or "#FFFFFF"
+            local text = escape(args.separator) or " "
+
+            titlebar:add(separator)
+
+            client.connect_signal("focus", function()
+                separator.markup = string.format("<span font='%s' foreground='%s'>%s</span>", font, color, text)
+            end)
+            client.connect_signal("unfocus", function() separator.text = "" end)
+        end
+    end
+
+    return titlebar
+end
+
+return fenetre
